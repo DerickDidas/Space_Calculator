1bc09dd1a4bfdd7c4b68726cf427246aa7e03ec5
DerickDidas <www.derickdidas1@gmail.com>
2024-07-15 16:55:42 +0800

    First Commit.

---
+++ lib/home_screen.dart
@@ -0,0 +1,211 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String userInput = "";
+  String result = "0";
+
+  List<String> buttonList = [
+    "AC",
+    "(",
+    ")",
+    "/",
+    "7",
+    "8",
+    "9",
+    "*",
+    "4",
+    "5",
+    "6",
+    "+",
+    "1",
+    "2",
+    "3",
+    "-",
+    "C",
+    "0",
+    ".",
+    "=",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Container(
+              height:
+                  MediaQuery.of(context).size.height / 3.95, //size ya screen
+              child: resultWidget(),
+            ),
+            Expanded(child: buttonWidget()),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget resultWidget() {
+    return Container(
+      color: Colors.black, //rangi ya screen
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Container(
+            padding: EdgeInsets.all(15),
+            alignment: Alignment.centerRight,
+            child: Text(
+              userInput,
+              style: TextStyle(
+                fontSize: 33,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.centerRight,
+            child: Text(
+              result,
+              style: TextStyle(
+                fontSize: 50,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buttonWidget() {
+    return Container(
+      padding: EdgeInsets.all(10),
+      color: Color.fromARGB(255, 1, 4, 9), // hio background
+      child: GridView.builder(
+        itemCount: buttonList.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 4,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemBuilder: (context, index) {
+          return button(buttonList[index]);
+        },
+      ),
+    );
+  }
+
+  getColor(String text) {
+    if (text == "/" ||
+        text == "*" ||
+        text == "+" ||
+        text == "-" ||
+        text == "C" ||
+        text == "(" ||
+        text == ")") {
+      return Colors.redAccent;
+    }
+    if (text == "=" || text == "AC") {
+      return Colors.white;
+    }
+    return Colors.white;
+    //rangi ya numbers
+  }
+
+  getBgColor(String text) {
+    if (text == "AC") {
+      return Colors.redAccent;
+    }
+    if (text == "=") {
+      return Color.fromARGB(255, 65, 137, 232);
+    }
+    return Colors.grey.withOpacity(0.1);
+    // hivyo viboxi rangi
+  }
+
+  Widget button(String text) {
+    return InkWell(
+      onTap: () {
+        setState(() {
+          handleButtonPress(text);
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: getBgColor(text),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              blurRadius: 1,
+              spreadRadius: 1,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: getColor(text),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  handleButtonPress(String text) {
+    if (text == "AC") {
+      userInput = "";
+      result = "0";
+      return;
+    }
+
+    if (text == "C") {
+      if (userInput.length > 0) {
+        userInput == userInput.substring(0, userInput.length - 1);
+        return;
+      } else {
+        return null;
+      }
+    }
+
+    if (text == "=") {
+      result = calculate();
+      userInput = result;
+      if (userInput.endsWith(".0")) {
+        userInput = userInput.replaceAll(".0", "");
+      }
+
+      if (result.endsWith(".0")) {
+        result = result.replaceAll(".0", "");
+      }
+      return;
+    }
+    userInput = userInput + text;
+  }
+
+  String calculate() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return "Error";
+    }
+  }
+}
